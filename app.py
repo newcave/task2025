2c9c1a7af6bc2e5cc6593e5ba9489a4f06fb93e7
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2025-03-05 10:32:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import subprocess
 GITHUB_REPO_OWNER = "newcave"  # GitHub 사용자 이름
 GITHUB_REPO_NAME = "task2025"  # GitHub 저장소 이름
 GITHUB_REPO_URL = f"https://github.com/{GITHUB_REPO_OWNER}/{GITHUB_REPO_NAME}"
-GITHUB_TOKEN = st.secrets[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"]  # Streamlit Secrets에서 GitHub 토큰.  Secrets 설정 확인!
+GITHUB_TOKEN = st.secrets["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"]  # Streamlit Secrets에서 GitHub 토큰.  Secrets 설정 확인!
 DATA_FILE = "tasks.csv"
 REQUIRED_COLUMNS = ["업무 제목", "업무 유형", "담당자", "마감일", "상태", "세부 내용", "등록일"]
 
